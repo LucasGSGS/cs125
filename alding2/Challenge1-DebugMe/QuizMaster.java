3c7ec312ce78d78c121816c36a26b77140548231
alding2 <alding2@67bb8810-f416-49f7-95ef-536b191976b8>
2013-09-14 02:10:05 +0000

    alding2

--- alding2/Challenge1-DebugMe/QuizMaster.java
+++ alding2/Challenge1-DebugMe/QuizMaster.java
@@ -3,7 +3,7 @@
  * A program to test students' CS knowledge.
  * Watch out ! There may be deliberate bugs in some of the logic below :-)
  * @see QuizMaster-ReadMe.txt for details on how to complete this program.
- * @author insert-your-netid-here
+ * @author alding2
  *
  */
 public class QuizMaster {
@@ -11,40 +11,45 @@ public class QuizMaster {
 	public static void main(String[] args) {
 		
 		System.out.println("Please enter your zip code.");
-		int zip = TextIO.getlnInt(); // Roughly, 61xxx-Illinois 48xxx = Michigan
+		String zip = TextIO.getln(); // Roughly, 61xxx-Illinois 48xxx = Michigan
 	
 		System.out.println("Which University CS Department was recently awarded $208 million to develop the worlds fastest computer?");
-		System.out.println("1. Illinois");
-		System.out.println("2. Michigan");
-		System.out.println("3. MIT");
+		System.out.println("1. Illinois\n2. Michigan\n3. MIT");
 		int gridComputing = TextIO.getlnInt();
 		
 		System.out.println("Which University CS Department designed and built the pioneering ILLIAC series?");
-		System.out.println("1. Illinois\n2. Wisconsin\n3. Berkeley\n");
+		System.out.println("1. Illinois\n2. Wisconsin\n3. Berkeley");
 		int illiac = TextIO.getlnInt();
-
-		System.out.println("Which University released \"Mosaic\" - the first multimedia cross-platform browser?");
+		
+		TextIO.putln("Which University released 'Mosaic' - the first multimedia cross-platform browser?");
 		System.out.println("(Mosaic's source code was later licensed to Microsoft and Netscape Communications)");
 		System.out.println("1. Illinois\n2. Michigan\n3. Wisconsin");
 		int mosaic = TextIO.getlnInt();
 		
-		System.out.println("True/False ? Variables have four things: a type, name, value and a memory location.");
-		System.out.println("1. True");
-		System.out.println("2. False");
+		System.out.println("True/False? Variables have four things: a type, name, value and a memory location.");
+		System.out.println("1. True\n2. False");
 		int varTF = TextIO.getlnInt();
 		
-		int score = 0;
-		/* Computer score: + 10 points for each correct answer */
+		int score = 0;                /* Computer score: + 10 points for each correct answer */
 		if(gridComputing==1) score = score + 10;
 		if(illiac==1) score = score + 10;
+		if(mosaic==1) score = score +10;
 		if(varTF==1) score = score + 10;
-		/* Michigan students (zip code 48xxx) need to get all of them correct to receive any points */
-		if(zip/1000 == 48) score =0;
 		
+		/* Michigan students (zip code 48xxx) need to get all of them correct to receive any points */
+		if((zip.charAt(0) == '4') && (zip.charAt(1) == '8')) {
+			if (score < 40) score = 0;
+			System.out.println("You scored:" + score);
+		}
 		/* If an Illinois student gets all questions correct print congratulations */
-		if(score == 40)
-			System.out.println("\nCongratulations!");
-		System.out.print("You scored:");
-		System.out.print(score);
+		if ((zip.charAt(0) == '6') && (zip.charAt(1) == '1')) {
+			if(score == 40) {
+				System.out.println("You scored:" + score); 
+				System.out.println("Congratulations!");
+			}
+			else if (score < 40) System.out.println("You scored:" + score);
+		}
+		else if ((zip.charAt(0) != '4') && (zip.charAt(1) != '6'))
+			System.out.println("You scored:" + score);
 	}
 }
