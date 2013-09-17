f8fbe19721bf7ce17b88d4b68541da368507c941
alding2 <alding2@67bb8810-f416-49f7-95ef-536b191976b8>
2013-09-17 20:59:25 +0000

    alding2

--- alding2/Challenge2-Hollywood/MovieSurvey.java
+++ alding2/Challenge2-Hollywood/MovieSurvey.java
@@ -3,17 +3,35 @@
  * A program to run a simple survey and report the results. See MovieSurvey.txt
  * for more information. TODO: add your netid to the line below
  * 
- * @author put-your-netid-here
+ * @author alding2
  */
 public class MovieSurvey {
 	public static void main(String[] arg) {
-		// TODO: Write your program here
-		// Hints :
-		// Formatted output
-		// http://math.hws.edu/javanotes/c2/s4.html#basics.4.4
-		
-		// Don't just copy paste the expected output
-		// For grading purposes your code may be tested
-		// with other input values.
+		int cinema;
+		int dvd;
+		int computer;
+		int total;
+		double cinemaFraction;
+		double notCinemaFraction;
+		TextIO.putln("Welcome. We're interested in how people are watching movies this year.");
+		TextIO.putln("Thanks for your time. - The WRITERS GUILD OF AMERICA.");
+		TextIO.putln("Please tell us about how you've watched movies in the last month.");
+		TextIO.putln("How many movies have you seen at the cinema?");
+		cinema = TextIO.getlnInt();
+		TextIO.putln("How many movies have you seen using a DVD or VHS player?");
+		dvd = TextIO.getlnInt();
+		TextIO.putln("How many movies have you seen using a Computer?");
+		computer = TextIO.getlnInt();
+		TextIO.putln("Summary: " + cinema + " Cinema movies, " + dvd + " DVD/VHS movies, " + computer + " Computer movies");
+		total = cinema + dvd + computer; 
+		TextIO.putln("Total: " + total + " movies");
+		cinemaFraction = (cinema/(double)total)*100.00;
+		TextIO.put("Fraction of movies seen at a cinema: ");
+		TextIO.putf("%.2f",cinemaFraction);
+		TextIO.putln("%");
+		notCinemaFraction = 100.00 - cinemaFraction;
+		TextIO.put("Fraction of movies seen outside of a cinema: ");
+		TextIO.putf("%.2f",notCinemaFraction);
+		TextIO.putln("%");
 	}
 }
