8842856eddfbf337de970893031e0bc0676d7d1b
alding2 <alding2@67bb8810-f416-49f7-95ef-536b191976b8>
2013-09-26 18:17:28 +0000

    commit1

--- alding2/Challenge3-TopSecret/CaesarCipher.java
+++ alding2/Challenge3-TopSecret/CaesarCipher.java
@@ -8,13 +8,44 @@
  * Good Programming Hints: "DRY: Don't Repeat Yourself"
  * Try to make your program as short as possible.
  * TODO: add your netid to the line below
- * @author put-your-netid-here
+ * @author alding2
  */
 public class CaesarCipher {
-
 	public static void main(String[] strings) {
-		//TODO: Delete the following line and write your implementation here- 
-		throw new RuntimeException ("Et tu Brutus?");
+		int offset = 0;
+		TextIO.putln("Please enter the shift value (between -25..-1 and 1..25)");
+		offset = TextIO.getlnInt();	
+		
+		while (offset <= -26 || offset == 0 || offset >= 26) {
+			TextIO.putln(offset + " is not a valid shift value.");
+			TextIO.putln("Please enter the shift value (between -25..-1 and 1..25)");
+			offset = TextIO.getlnInt();
+		}		
+		TextIO.putln("Using shift value of " + offset);		
+		TextIO.putln("Please enter the source text (empty line to quit)");
+		String line = TextIO.getln();		
+		while(!line.equals("")) {
+			TextIO.putln("Source   :" + line);		
+			line = line.toUpperCase();		      
+			String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+			int i;                                                 // stepper for alphabet array
+			int k;                                                 // stepper for line
+			String result = "";
+			for (k=0; k < line.length(); k++) {
+				for (i=0; i < alphabet.length(); i++) {
+					int letter = 0;
+					if (line.charAt(k) == alphabet.charAt(i)) {
+						letter = i + offset;
+						if(letter > 26 || letter < 0) letter = (letter+26)%26;
+						result = result + alphabet.charAt(letter);
+					}
+				}
+				
+			}
+			TextIO.putln("Processed:" + result);
+			TextIO.putln("Please enter the source text (empty line to quit)");
+			line = TextIO.getln();
+		}
+		TextIO.putln("Bye.");
 	}
-
 }
